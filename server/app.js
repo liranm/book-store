51014e1d3edc41c7b931d67902f5343135986ebf
Liran Mery <liran135@gmail.com>
2019-02-03 13:40:01 +0200

    list books

--- server/app.js
+++ server/app.js
@@ -8,18 +8,10 @@ const port = 3000;
 
 mongoose.connect('mongodb://localhost:27017/books', {useNewUrlParser: true});
 
-const book = new Book({
-    title: 'The book',
-    description: 'Some book blablabla',
-    isbn: '1233456471',
-    author: 'Liran',
-    publication_data: '2018-12-04',
-    genre: 'Horrow',
-    price: 45.5
+app.get('/', async (req, res) => {
+    const books = await Book.find();
+
+    res.json(books);
 });
-
-book.save().then(() => console.log('new book created'));
-
-app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
