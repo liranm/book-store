b32de2c80a442dddd542f33df3526f3c5f5a1c95
Liran Mery <liran135@gmail.com>
2019-02-04 10:44:56 +0200

    remove book in client

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -19,12 +19,43 @@ class BookList extends Component {
         });
     }
 
+    handleRemove = (event) => {
+        this.removeBook(this.state.chosenBook);
+    }
+
     componentDidMount() {
         fetch('http://localhost:3000')
             .then(response => response.json())
             .then((books) => {
                 this.setState({ books });
             });
+    }
+
+    async removeBook(book) {
+        const { isbn } = book;
+
+        const res = await fetch(
+            'http://localhost:3000/remove',
+            {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                method: 'DELETE',
+                body: JSON.stringify({ isbn })
+            }
+        );
+
+        if(res.status === 200) {
+            const removedBookIndex = this.state.books.indexOf(book);
+
+            this.setState({
+                books: [
+                    ...this.state.books.slice(0, removedBookIndex),
+                    ...this.state.books.slice(removedBookIndex +1)
+                ],
+                chosenBook: null
+            });
+        }
     }
 
     getBookLinks(books) {
@@ -51,6 +82,14 @@ class BookList extends Component {
             <div>
                 {this.getBookLinks(this.state.books)}
                 {this.state.chosenBook && <Book {...this.state.chosenBook} />}
+                {this.state.chosenBook && 
+                    <button
+                        type="button"
+                        onClick={this.handleRemove}
+                    >
+                        Remove book
+                    </button>
+                }
             </div>
         );
     }
