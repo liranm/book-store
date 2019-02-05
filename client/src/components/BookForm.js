e81ce087152e30f391d924023f83571c9b1b0199
Liran Mery <liran135@gmail.com>
2019-02-05 11:40:15 +0200

    style book form

--- client/src/components/BookForm.js
+++ client/src/components/BookForm.js
@@ -54,41 +54,82 @@ class BookForm extends Component {
 
     render() {
         return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>Title</label>
-                    <input type="text" value={this.state.title} onChange={this.handleChange} name="title" />
+            <form
+                onSubmit={this.handleSubmit}
+                className="bookForm"
+            >
+                <label>Title</label>
+                <input
+                    type="text"
+                    value={this.state.title}
+                    onChange={this.handleChange}
+                    name="title"
+                    className="bookForm__input"
+                />
 
-                    <label>Description</label>
-                    <input type="text" value={this.state.description} onChange={this.handleChange} name="description" />
+                <label>Description</label>
+                <input
+                    type="text"
+                    value={this.state.description}
+                    onChange={this.handleChange}
+                    name="description"
+                    className="bookForm__input"
+                />
 
-                    <label>ISBN</label>
-                    <input type="text" value={this.state.isbn} onChange={this.handleChange} name="isbn" />
+                <label>ISBN</label>
+                <input
+                    type="text"
+                    value={this.state.isbn}
+                    onChange={this.handleChange}
+                    name="isbn"
+                    className="bookForm__input"
+                />
 
-                    <label>Author</label>
-                    <input type="text" value={this.state.author} onChange={this.handleChange} name="author" />
+                <label>Author</label>
+                <input
+                    type="text"
+                    value={this.state.author}
+                    onChange={this.handleChange}
+                    name="author"
+                    className="bookForm__input"
+                />
 
-                    <label>Publication Date</label>
-                    <input type="date" value={this.state.publicationDate} onChange={this.handleChange} name="publicationDate" />
+                <label>Publication Date</label>
+                <input
+                    type="date"
+                    value={this.state.publicationDate}
+                    onChange={this.handleChange}
+                    name="publicationDate"
+                    className="bookForm__input"
+                />
 
-                    <label>Genre</label>
-                    <select value={this.state.genre} onChange={this.handleChange} name="genre">
-                        <option value="">--Select--</option>
-                        <option value="science_fiction">Science fiction</option>
-                        <option value="satire">Satire</option>
-                        <option value="drama">Drama</option>
-                        <option value="action">Action</option>
-                        <option value="romance">Romance</option>
-                        <option value="mystery">Mystery</option>
-                        <option value="horror">Horror</option>
-                    </select>
+                <label>Price</label>
+                <input
+                    type="number"
+                    value={this.state.price}
+                    onChange={this.handleChange}
+                    name="price"
+                    className="bookForm__input"
+                />
 
-                    <label>Price</label>
-                    <input type="number" value={this.state.price} onChange={this.handleChange} name="price" />
+                <label>Genre</label>
+                <select
+                    value={this.state.genre}
+                    onChange={this.handleChange}
+                    name="genre"
+                >
+                    <option value="">--Select--</option>
+                    <option value="science_fiction">Science fiction</option>
+                    <option value="satire">Satire</option>
+                    <option value="drama">Drama</option>
+                    <option value="action">Action</option>
+                    <option value="romance">Romance</option>
+                    <option value="mystery">Mystery</option>
+                    <option value="horror">Horror</option>
+                </select>
 
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
+                <button type="submit">Submit</button>
+            </form>
         );
     }
 }
