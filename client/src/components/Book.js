31b0b22b23a6b7d44e9dfa1f50dde4d51b4155e4
Liran Mery <liran135@gmail.com>
2019-02-06 09:55:13 +0200

    style book

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -3,13 +3,20 @@ import React from 'react';
 const Book = ({ title, description, isbn, author, publication_date, genre, price }) => {
     return (
         <div className="book">
-            <h1>{title}</h1>
-            <h2>{description}</h2>
-            <p>{isbn}</p>
-            <p>{author}</p>
-            <p>{publication_date}</p>
-            <p>{genre}</p>
-            <p>{price}</p>
+            <label className="book__label">Title</label>
+            <h1 className="book__title">{title}</h1>
+            <label className="book__label">Description</label>
+            <h2 className="book__subtitle">{description}</h2>
+            <label className="book__label">ISBN</label>
+            <p className="book__field">{isbn}</p>
+            <label className="book__label">Author</label>
+            <p className="book__field">{author}</p>
+            <label className="book__label">Publication Date</label>
+            <p className="book__field">{new Date(publication_date).toDateString()}</p>
+            <label className="book__label">Genre</label>
+            <p className="book__field">{genre}</p>
+            <label className="book__label">Price</label>
+            <p className="book__field">{price.toFixed(2)} $</p>
         </div>
     );
 };
